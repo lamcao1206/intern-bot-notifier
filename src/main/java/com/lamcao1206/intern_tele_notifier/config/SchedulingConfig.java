42fc09e209664283c9ad09f81c9e9c0517b59012
Cao Ngoc Lam <lam.cao1206@hcmut.edu.vn>
2025-04-09 14:08:27 +0700

    create scheduling configuration

---
+++ src/main/java/com/lamcao1206/intern_tele_notifier/config/SchedulingConfig.java
@@ -0,0 +1,18 @@
+package com.lamcao1206.intern_tele_notifier.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulingConfig {
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("job-tracker-");
+        scheduler.initialize();
+        return scheduler;
+    }
+}
